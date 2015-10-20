9fdf1c760a012e33537c16d0e8b0f9d6cd071e32
Andriy Kovalchuk <tulsluper@gmail.com>
2015-10-20 15:23:01 +0300

    added postgres init script

---
+++ configs/db_init_postgres.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE sanscriptdb;
+CREATE USER sanscriptdbuser WITH PASSWORD 'sanscriptdbpass';
+GRANT ALL PRIVILEGES ON DATABASE sanscriptdb to sanscriptdbuser;
--- sanscript/settings.py
+++ sanscript/settings.py
@@ -86,8 +86,16 @@ WSGI_APPLICATION = 'sanscript.wsgi.application'
 
 DATABASES = {
     'default': {
+
+        # comment out lines below for sqlite3
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # comment out lines below for postgresql
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'sanscriptdb',
+        # 'USER': 'sanscriptdbuser',
+        # 'PASSWORD': 'sanscriptdbpass'
     }
 }
 
