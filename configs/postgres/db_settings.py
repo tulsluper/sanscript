8025c41b3a0b708d753790acd7739726f83e1856
tulsluper@gmail.com <tulsluper@gmail.com>
2015-11-17 12:38:40 +0200

    postgres setting to seperate file

---
+++ configs/postgres/db_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        # PostgreSQL
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sandb',
+        'USER': 'sandbuser',
+        'PASSWORD': 'sandbpass',
+        'HOST':'localhost',
+        'PORT':'',
+    }
+}
