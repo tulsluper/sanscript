bf34dd3692c572fd3de9d4ade916aff3fb5ae9f4
tulsluper@gmail.com <tulsluper@gmail.com>
2015-11-17 12:43:40 +0200

    postgres config to separate file

--- sanscript/settings.py
+++ sanscript/settings.py
@@ -15,6 +15,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+try:
+    from db_settings import DATABASES
+except:
+    DATABASES = None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -84,23 +88,14 @@ WSGI_APPLICATION = 'sanscript.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-
-        # comment out lines below for sqlite3
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-        # comment out lines below for postgresql
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'sanscriptdb',
-        #'USER': 'sanscriptdbuser',
-        #'PASSWORD': 'sanscriptdbpass',
-        #'HOST':'localhost',
-        #'PORT':'',
+if DATABASES is None:
+    DATABASES = {
+        'default': {
+            # SQLite
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
