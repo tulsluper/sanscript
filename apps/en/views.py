174da98ea5825945d1f0efb24cc3ee23c15c5c61
tulsluper@gmail.com <tulsluper@gmail.com>
2016-02-23 09:18:47 +0200

    views for en

--- apps/en/views.py
+++ apps/en/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
+from apps.sa.defs import sfilter, stable
+from .models import *
+
 
 def home(request):
     return render(request, 'home.html')
+
+
+def enclosures(request):
+    objs = sfilter(Enclosure, request)
+    cols, rows = stable(Enclosure, objs)
+    data = {
+        'cols': cols,
+        'rows': rows,
+    }
+    return render(request, 'table.html', data)
+
+
+def servers(request):
+    objs = sfilter(Server, request)
+    cols, rows = stable(Server, objs)
+    data = {
+        'cols': cols,
+        'rows': rows,
+    }
+    return render(request, 'table.html', data)
+
+
+def mezzanines(request):
+    objs = sfilter(Mezzanine, request)
+    cols, rows = stable(Mezzanine, objs)
+    data = {
+        'cols': cols,
+        'rows': rows,
+    }
+    return render(request, 'table.html', data)
+
